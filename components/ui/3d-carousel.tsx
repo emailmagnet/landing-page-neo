502e31fa5dcc9b41e8129f249ad8e5a3e83bbc5f
emailmagnet <team@emailmagnet.ai>
2026-01-20 16:24:22 +0100

    remove modal on 3d gallery

--- components/ui/3d-carousel.tsx
+++ components/ui/3d-carousel.tsx
@@ -81,12 +81,10 @@ const transitionOverlay: any = { duration: 0.5, ease: [0.32, 0.72, 0, 1] }
 
 const Carousel = memo(
   ({
-    handleClick,
     controls,
     cards,
     isCarouselActive,
   }: {
-    handleClick: (imgUrl: string, index: number) => void
     controls: any
     cards: string[]
     isCarouselActive: boolean
@@ -147,7 +145,6 @@ const Carousel = memo(
                 transform: `rotateY(${i * (360 / faceCount)
                   }deg) translateZ(${radius}px)`,
               }}
-              onClick={() => handleClick(imgUrl, i)}
             >
               <motion.img
                 src={imgUrl}
@@ -170,7 +167,6 @@ const Carousel = memo(
 const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`
 const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`
 function ThreeDPhotoCarousel({ images }: { images?: string[] }) {
-  const [activeImg, setActiveImg] = useState<string | null>(null)
   const [isCarouselActive, setIsCarouselActive] = useState(true)
   const controls = useAnimation()
   const cards = useMemo(
@@ -182,53 +178,10 @@ function ThreeDPhotoCarousel({ images }: { images?: string[] }) {
     console.log("Cards loaded:", cards)
   }, [cards])
 
-  const handleClick = (imgUrl: string) => {
-    setActiveImg(imgUrl)
-    setIsCarouselActive(false)
-    controls.stop()
-  }
-
-  const handleClose = () => {
-    setActiveImg(null)
-    setIsCarouselActive(true)
-  }
-
   return (
     <motion.div layout className="relative">
-      <AnimatePresence mode="sync">
-        {activeImg && (
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0 }}
-            layoutId={`img-container-${activeImg}`}
-            layout="position"
-            onClick={handleClose}
-            className="fixed inset-0 bg-black bg-opacity-10 flex items-center justify-center z-50 m-5 md:m-36 lg:mx-[19rem] rounded-3xl"
-            style={{ willChange: "opacity" }}
-            transition={transitionOverlay}
-          >
-            <motion.img
-              layoutId={`img-${activeImg}`}
-              src={activeImg}
-              className="max-w-full max-h-full rounded-lg shadow-lg"
-              initial={{ scale: 0.5 }} // Start with a smaller scale
-              animate={{ scale: 1 }} // Animate to full scale
-              transition={{
-                delay: 0.5,
-                duration: 0.5,
-                ease: [0.25, 0.1, 0.25, 1],
-              }} // Clean ease-out curve
-              style={{
-                willChange: "transform",
-              }}
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
       <div className="relative h-[500px] w-full overflow-hidden">
         <Carousel
-          handleClick={handleClick}
           controls={controls}
           cards={cards}
           isCarouselActive={isCarouselActive}
